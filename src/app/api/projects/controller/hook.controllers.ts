4bd715fd53c8324ec0d66cf61f33ebed8e96dc4c
FAIZULLAH <faizullah9181@gmail.com>
2023-11-26 22:57:26 +0530

    Refactor webhook redirect URL to include query parameters

--- src/app/api/projects/controller/hook.controllers.ts
+++ src/app/api/projects/controller/hook.controllers.ts
@@ -15,6 +15,44 @@ export default class HookController extends MasterController {
 
     async controller() {
         console.log('WEBHOOK', JSON.stringify(this.data));
-        return this.res.redirect(301, 'https://fozdeal-client.netlify.app/');
+
+        const {
+            MID,
+            ORDERID,
+            RESPCODE,
+            RESPMSG,
+            STATUS,
+            TXNAMOUNT,
+            issandboxmode,
+            transaction_number,
+            transaction_status,
+            website_ref_no,
+            checksumhash
+        } = this.data;
+        const url =
+            'https://fozdeal-client.netlify.app?MID=' +
+            MID +
+            '&ORDERID=' +
+            ORDERID +
+            '&RESPCODE=' +
+            RESPCODE +
+            '&RESPMSG=' +
+            RESPMSG +
+            '&STATUS=' +
+            STATUS +
+            '&TXNAMOUNT=' +
+            TXNAMOUNT +
+            '&issandboxmode=' +
+            issandboxmode +
+            '&transaction_number=' +
+            transaction_number +
+            '&transaction_status=' +
+            transaction_status +
+            '&website_ref_no=' +
+            website_ref_no +
+            '&checksumhash=' +
+            checksumhash;
+
+        return this.res.redirect(301, url);
     }
 }
