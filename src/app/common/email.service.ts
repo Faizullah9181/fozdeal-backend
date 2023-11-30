57531348e79e6e00676279118366829257376730
FAIZULLAH <faizullah9181@gmail.com>
2023-12-01 02:22:48 +0530

    Update button text in email templates

--- src/app/common/email.service.ts
+++ src/app/common/email.service.ts
@@ -30,7 +30,7 @@ class GridEmailService {
                 <div class="container">
                     <h1>Forgot Password Email</h1>
                     <p>You are receiving this email because we received a password reset request for your account.</p>
-                    <p>Click the button below to reset your password:</p>
+                    <p>Click on the follwing button given below to reset your password:</p>
                     <a href="${process.env.DOMAIN_URL}/reset-password/${token}">Reset Password</a>
                 </div>
 
