8b537505105c38c632018e2aeb369cbc4fc116e9
FAIZULLAH <faizullah9181@gmail.com>
2023-11-30 17:54:57 +0530

    Fix gender field validation

--- src/app/api/auth/user/controllers/user.register.controller.ts
+++ src/app/api/auth/user/controllers/user.register.controller.ts
@@ -33,7 +33,7 @@ export default class UserRegisterController extends MasterController {
                 last_name: Joi.string().required(),
                 email: Joi.string().email().required(),
                 password: Joi.string().min(6).max(20).required(),
-                gender: Joi.string().required(),
+                gender: Joi.string(),
                 user_type: Joi.string()
                     .valid(Roles.ENTERPRENEUR, Roles.INVESTOR)
                     .required(),
